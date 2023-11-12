89b4e86c249cba70e618244860b1c845c941bf4b
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-12 21:33:57 +0300

    adding util mongodb function gor find or create document

---
+++ Backend/util/findOrCreateDocument.js
@@ -0,0 +1,9 @@
+const findOrCreateDocument = async (id) => {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+};
+
+module.exports = findOrCreateDocument;
