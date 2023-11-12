f7d30e36b61867db39c2d8791981f4d37c97f688
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-12 22:31:46 +0300

    adding use document hook that interact with our socket

---
+++ googledoc/src/hooks/useDocument.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+const useDocument = (socket, documentId, quill) => {
+  useEffect(() => {
+    if (socket == null) return;
+
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    socket.emit("get-document", documentId);
+  }, [socket, documentId, quill]);
+
+  useEffect(() => {
+    if (socket == null) return;
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
+};
+
+export default useDocument;
