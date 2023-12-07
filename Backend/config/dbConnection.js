dbb259a56340e4bd4e7ebc5a04de5c2ce517901e
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-08 02:32:44 +0300

    fixing bugs and change db configration

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDatabase() {
-  mongoose.connect("", {
+  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
