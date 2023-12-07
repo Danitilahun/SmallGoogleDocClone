3e7d01b38aed7790291bf568f5cf8718f5ecd8cc
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-07 22:16:10 +0300

    fix error

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 function connectDatabase() {
-  mongoose.connect(
-    "mongodb+srv://danielT:7fXyu7qPaoEORE4e@cluster0.3vmplwh.mongodb.net/google-docs-clone?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    }
-  );
+  mongoose.connect("", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   return mongoose.connection;
 }
