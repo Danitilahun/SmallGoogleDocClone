fc5c785668eac2095b45f4f20d784ba1a5a8fa23
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-08 03:32:13 +0300

    fixing errors

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 function connectDatabase() {
-  mongoose.connect(
-    "mongodb+srv://progressma92:qRJHN2jJb2yEErj6@cluster0.aemyxz3.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   return mongoose.connection;
 }
